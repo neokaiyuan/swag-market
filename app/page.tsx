eae13f06ea1fec00273a4e281bf65c610e74212e
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-01-15 14:38:45 +0800

    Strikeout listing on home page after sold

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,28 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
   const supabase = await createClient();
-  const { data: listings } = await supabase.from("listings").select();
+  const { data: listings } = await supabase
+    .from("listings")
+    .select("id, title, buyer_id");
 
   return (
     <div className="min-h-screen bg-gray-900 text-white flex justify-center">
       <main className="p-8 flex flex-col items-center gap-4">
         {listings &&
-          listings.map((listing) => (
-            <Link key={listing.id} href={`/listing/${listing.id}`}>
-              <div>{listing.title}</div>
-            </Link>
-          ))}
+          listings.map((listing) => {
+            const listingTitle = listing.buyer_id ? (
+              <span style={{ textDecoration: "line-through" }}>
+                [SOLD] {listing.title}
+              </span>
+            ) : (
+              <span>{listing.title}</span>
+            );
+            return (
+              <div key={listing.id}>
+                <Link href={`/listing/${listing.id}`}>{listingTitle}</Link>
+              </div>
+            );
+          })}
       </main>
     </div>
   );
