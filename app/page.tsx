02bfc94162302df30868086f4d34e7f9bc690882
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-01-15 19:42:49 +0800

    Render listings with images in grid format and show sold listings at bottom on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,45 @@
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
 
 export default async function Home() {
   const supabase = await createClient();
   const { data: listings } = await supabase
     .from("listings")
-    .select("id, title, buyer_id");
+    .select("id, title, buyer_id, image_url");
+
+  const sortedListings = listings?.sort((a) => (a.buyer_id ? 1 : -1));
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex justify-center">
-      <main className="p-8 flex flex-col items-center gap-4">
-        {listings &&
-          listings.map((listing) => {
-            const listingTitle = listing.buyer_id ? (
-              <span style={{ textDecoration: "line-through" }}>
+    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-start">
+      <main className="p-8 grid grid-cols-2 gap-2">
+        {sortedListings &&
+          sortedListings.map((listing) => {
+            const isSold = !!listing.buyer_id;
+            const listingTitle = isSold ? (
+              <span style={{ textDecoration: "line-through", color: "gray" }}>
                 [SOLD] {listing.title}
               </span>
             ) : (
               <span>{listing.title}</span>
             );
             return (
-              <div key={listing.id}>
-                <Link href={`/listing/${listing.id}`}>{listingTitle}</Link>
-              </div>
+              <Link href={`/listing/${listing.id}`} key={listing.id}>
+                <div
+                  className={`p-4 bg-gray-800 text-white rounded shadow-md ${
+                    isSold ? "opacity-50" : ""
+                  }`}
+                >
+                  <Image
+                    src={listing.image_url}
+                    alt={listing.title}
+                    width={500}
+                    height={200}
+                    className="w-full h-32 object-cover mb-2 rounded"
+                  />
+                  {listingTitle}
+                </div>
+              </Link>
             );
           })}
       </main>
